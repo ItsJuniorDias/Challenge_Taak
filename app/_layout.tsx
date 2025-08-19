ffa010db74c4729dc2f2bf471852397d6794362e
Alexandre <reabilitado97@gmail.com>
2025-08-19 15:23:13 -0300

    chore: create eas file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
+import "react-native-reanimated";
+
 import React from "react";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
+import * as SplashScreen from "expo-splash-screen";
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const [loaded] = useFonts({
