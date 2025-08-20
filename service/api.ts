f9cf5ee24ce0d7e1fe8f15f043b5a8fe0ed6272d
Alexandre <reabilitado97@gmail.com>
2025-08-20 17:35:14 -0300

    chore: adjust ios url

--- service/api.ts
+++ service/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
 export const api = axios.create({
-  baseURL: "http://10.0.2.2:3000",
+  baseURL:
+    Platform.OS === "android"
+      ? "http://10.0.2.2:3000"
+      : "http://localhost:3000",
   timeout: 1000,
 });
